dc2647c63bdb998e54246f7e83db4569e2ff2dae
shazam <shazajmal37@gmail.com>
2024-03-02 22:04:27 +0530

    Pipeline blueprint

---
+++ run_pipeline.py
@@ -0,0 +1,6 @@
+from pipeline.training_pipeline import training_pipeline
+
+if __name__ == "__main__":
+    # run the pipeline
+    training_pipeline(data_path="data/olist_customers_dataset.csv")
+    
---
+++ steps/ingest_data.py
@@ -0,0 +1,37 @@
+import logging
+
+import pandas as pd
+from zenml import step
+
+class IngestData:
+    """
+    Ingesting the data from the data path
+    """
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+    """
+    Args:
+        data: path to the data 
+    """
+    def get_data(self):
+        logging.info(f"Ingesting data from {self.data_path}")
+        return pd.read_csv(self.data_path)
+    
+@step
+def ingest_data(data_path: str) -> pd.DataFrame:
+    """
+    Ingesting the data from the data_path
+
+    Args:
+        data_path: path to the data
+    Returns:
+        pd.DataFrame: the ingested data     
+    """
+    try:
+        ingest_data = IngestData(data_path)
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(f"Error while ingesting data: {e}")
+        raise e
+
